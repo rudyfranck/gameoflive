34375f07f4e0bb317f8c83196ec27c21afc05e03
Rudy Franck <zepequenoledemysocle@gmail.com>
2019-07-15 10:03:26 +0300

    Initial Commit

---
+++ build/precache-manifest.dc7b96fdf1810211568db3078980e450.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9db7b33bc59a4a21d811",
+    "url": "/rudylino/gameoflive.git/static/css/main.d8e2eef6.chunk.css"
+  },
+  {
+    "revision": "9db7b33bc59a4a21d811",
+    "url": "/rudylino/gameoflive.git/static/js/main.3a47611c.chunk.js"
+  },
+  {
+    "revision": "0dfedf9aaf08380ee98e",
+    "url": "/rudylino/gameoflive.git/static/js/runtime~main.7a81c55c.js"
+  },
+  {
+    "revision": "d1d6e4ed0356f93c3c2c",
+    "url": "/rudylino/gameoflive.git/static/js/2.6a9413c7.chunk.js"
+  },
+  {
+    "revision": "cb45fa32d3fe436ede84d05ab2dba9f2",
+    "url": "/rudylino/gameoflive.git/index.html"
+  }
+];
